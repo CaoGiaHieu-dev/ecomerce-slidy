0cb3ed7574c040b51064d2e23384685f91c08fc0
CaoGiaHieu-dev <caogiahieu99@gmail.com>
2021-01-18 14:45:41 +0700

    modifi and upgrade flutter version

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,5 @@
 import 'package:ecomerce/app/modules/detail/detail_bloc.dart';
+
 import 'package:ecomerce/app/app_bloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget 
-{
+class HomePage extends StatefulWidget {
   final String title;
-  const HomePage({Key key, this.title = "Ecomerce"}) : super(key: key);
+  const HomePage({Key key, this.title = "Home"}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> 
-{
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-    (
-      appBar: AppBar
-      (
+    return Scaffold(
+      appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column
-      (
+      body: Column(
         children: <Widget>[],
       ),
     );
