0820c3e15b8dd4bee9782a3b92c7209cc930fcc5
DESKTOP-EV7AHHI\Shjn <caogiahieu99@gmail.com>
2021-01-18 10:55:49 +0700

    first commit

---
+++ lib/app/modules/detail/detail_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  final String title;
+  const DetailPage({Key key, this.title = "Detail"}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
