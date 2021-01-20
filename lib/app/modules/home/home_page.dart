570c25125dbffa9f030ef3f08d29a8a3dd7e603d
CaoGiaHieu-dev <caogiahieu99@gmail.com>
2021-01-20 13:20:51 +0700

    fix bug column

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:ecomerce/app/modules/home/widgets/category/category_page.dart';
 import 'package:ecomerce/app/modules/home/widgets/header/header_page.dart';
+import 'package:ecomerce/app/modules/home/widgets/hot_items/hot_items_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -32,69 +34,99 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
       //   title: Text(widget.title),
       //   backgroundColor: kMainColor,
       // ),
-      body: Stack
+      body: SingleChildScrollView
       (
-        children: <Widget>
-        [
-          HeaderPage(),
-          SafeArea
-          (
-            child: Column
+        child: Stack
+        (
+          children: <Widget>
+          [
+            HeaderPage(),
+            SafeArea
             (
-              children: <Widget>
-              [
-                Padding
-                (
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row
+              child: Column
+              (
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children : <Widget>
+                [
+                  Padding
                   (
-                    children: <Widget>
-                    [
-                      MaterialButton
-                      (
-                        padding: const EdgeInsets.all(8.0),
-                        shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0)),
-                        child: Icon(Icons.arrow_back_ios),
-                        color: Colors.white,
-                        textColor: Colors.black,
-                        minWidth: 0,
-                        height: 40,
-                        onPressed: () => null,
-                      ),
-                    ]
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row
+                    (
+                      children: <Widget>
+                      [
+                        MaterialButton
+                        (
+                          padding: const EdgeInsets.all(8.0),
+                          shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                          child: Icon(Icons.arrow_back_ios),
+                          color: HexColor("#f2eaec"),
+                          textColor: Colors.black,
+                          minWidth: 0,
+                          height: 40,
+                          onPressed: () => null,
+                        ),
+                      ]
+                    ),
                   ),
-                ),
-                SizedBox
-                (
-                  height: MediaQuery.of(context).size.height * 0.25,
-                ),
-                Expanded
-                (
-                  child: Container
+                  SizedBox
                   (
-                    height: MediaQuery.of(context).size.height +200,
+                    height: MediaQuery.of(context).size.height * 0.25,
+                  ),
+                  Container
+                  (
+                    // height: 300.0 *6 - 200,
                     decoration: BoxDecoration
                     (
                       borderRadius: BorderRadius.circular(20.0),
-                      color: Colors.white
+                      color: HexColor("#f2eaec")
                     ),
-                    child: SingleChildScrollView
+                    child: Column
                     (
-                      child: Column
-                      (
-                        children: <Widget>
-                        [
-                          CategoryPage()
-                        ],
-                      ),
+                      children: <Widget>
+                      [
+                        CategoryPage(),
+                        HotItemsPage()
+                        // Expanded
+                        // (
+                        //   child: HotItemsPage()
+                        // )
+                        // HotItemsPage(),
+                      ],
                     ),
                   )
-                )
-              ]
+                  // Expanded
+                  // (
+                  //   child: Container
+                  //   (
+                  //     height: MediaQuery.of(context).size.height +200,
+                  //     decoration: BoxDecoration
+                  //     (
+                  //       borderRadius: BorderRadius.circular(20.0),
+                  //       color: Colors.white
+                  //     ),
+                  //     child: Column
+                  //     (
+                  //       mainAxisSize: MainAxisSize.min,
+                  //       children: <Widget>
+                  //       [
+                  //         CategoryPage(),
+                  //         Expanded
+                  //         (
+                  //           child: HotItemsPage(),
+                  //         )
+                  //         
+                  //       ],
+                  //     ),
+                  //   )
+                  // )
+                ]
+              )
             )
-          )
-        ],
+          ],
+        ),
       )
     );
   }
